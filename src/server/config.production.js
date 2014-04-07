d2a26ebd36e195ae4757045040d52ad48268083e
Christo Fogelberg <cgf@syntilect.com>
2014-04-07 03:46:13 +0100

    node-error-server --> simple-error-server; server/package.json private false --> true

--- src/server/config.production.js
+++ src/server/config.production.js
@@ -6,12 +6,12 @@ module.exports = {
 	http_port: 17394,
 	database_host: "localhost",
 	database_port: "27017",
-	database: "node-error-server",
+	database: "simple-error-server",
 	server_root: __dirname,
 	client_root: path.join(__dirname, "../client"),
 	express_logger_format: ":remote-addr - - [:date] \":method :url HTTP/:http-version\" :status \":referrer\" \":user-agent\"",
 	email: {
-		from: "\"Node Error Server\" <node-error-server@yourdomain.com>",
+		from: "\"Simple Error Server\" <simple-error-server@yourdomain.com>",
 		to: "engineering@yourdomain.com",
 		bcc: "needs-to-know@yourdomain.com",
 		gap: 60 * 1000, // Don't send another email of the same type within this time frame
