93a205f8f418fd7076f4c00db66a44c38061a260
Christo Fogelberg <cgf@syntilect.com>
2014-06-03 21:25:08 +0100

    Gruntfile.js, package.json - using grunt-set-app-mode and grunt-synchard; general Gruntfile.js tidyup

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,34 +1,19 @@
-module.exports = function(grunt){
+module.exports = function(grunt) {
     var _ = require("underscore");
-
-    var build_tasks = _.filter(grunt.cli.tasks, function(arg) {
-        return arg.indexOf("build:") !== -1;
-    });
-    var build_modes = _.map(build_tasks, function(task) {
-        return task.substring(task.indexOf(":") + 1);
-    });
-    if(build_modes.length > 1) {
-        grunt.log.error("Multiple build tasks specified: " + JSON.stringify(build_tasks));
-    }
-    else {
-        var build_mode = build_modes[0];
-    }
-
     require("matchdep").filterDev("grunt-*").forEach(grunt.loadNpmTasks);
+
+    var mode = grunt.option("mode") || "dev";
 
     grunt.initConfig({
         // Configuration files and variables
         pkg: grunt.file.readJSON("package.json"),
         app_info: grunt.file.readJSON("app_info.json"),
-        build_mode: build_mode,
+        mode: mode,
 
         // Plugin tasks
         clean: {
             total: {
-                src: [ "build" ]
-            },
-            tidy: {
-                src: [ "build/**/*.dev.*", "build/**/*.staging.*", "build/**/*.prod.*" ]
+                src: ["build"]
             }
         },
 
@@ -36,59 +21,27 @@ module.exports = function(grunt){
         copy: {
             build: {
                 cwd: "src",
-                src: [ "**" ],
+                src: ["**"],
                 dest: "build",
                 expand: true,
                 mode: true
-            },
-            config_dev: {
-                nonull: true,
-                cwd: "src",
-                src: "src/server/config.dev.js",
-                dest: "build/server/config.js",
-                mode: true
-            },
-            config_staging: {
-                nonull: true,
-                cwd: "src",
-                src: "src/server/config.staging.js",
-                dest: "build/server/config.js",
-                mode: true
-            },
-            config_prod: {
-                nonull: true,
-                cwd: "src",
-                src: "src/server/config.prod.js",
-                dest: "build/server/config.js",
-                mode: true
-            },
-            override_dev: {
-                nonull: true,
-                cwd: "src",
-                src: "src/scripts/upstart/simple-error-server.dev.override",
-                dest: "build/scripts/upstart/simple-error-server.override",
-                mode: true
-            },
-            override_staging: {
-                nonull: true,
-                cwd: "src",
-                src: "src/scripts/upstart/simple-error-server.staging.override",
-                dest: "build/scripts/upstart/simple-error-server.override",
-                mode: true
-            },
-            override_prod: {
-                nonull: true,
-                cwd: "src",
-                src: "src/scripts/upstart/simple-error-server.prod.override",
-                dest: "build/scripts/upstart/simple-error-server.override",
-                mode: true
+            }
+        },
+
+        set_app_mode: {
+            config: {
+                expected_modes: ["dev", "staging", "prod"],
+                files: [{
+                    src: "src/server/config.{{MODE}}.js",
+                    dest: "build/server"
+                }]
             }
         },
 
         mkdir: {
             logs: {
                 options: {
-                    create: [ "build/server/logs" ]
+                    create: ["build/server/logs"]
                 }
             }
         },
@@ -97,55 +50,55 @@ module.exports = function(grunt){
             js: {
                 options: {
                     banner: "/*\n" +
-                    " * <%= app_info.name %> - version <%= app_info.version %>:<%= build_mode %> - " +
-                    "<%= grunt.template.today('yyyy-mm-dd') %>\n" +
-                    " * <%= app_info.description %>\n" +
-                    " * (C) <%= grunt.template.today('yyyy') %> <%= app_info.author %>\n" +
-                    " */\n"
+                        " * <%= app_info.name %> - version <%= app_info.version %>:<%= mode %> - " +
+                        "<%= grunt.template.today('yyyy-mm-dd') %>\n" +
+                        " * <%= app_info.description %>\n" +
+                        " * (C) <%= grunt.template.today('yyyy') %> <%= app_info.author %>\n" +
+                        " */\n"
                 },
                 files: {
-                    src: [ "build/**/*.js" ]
+                    src: ["build/**/*.js"]
                 }
             },
 
             css: {
                 options: {
                     banner: "/*\n" +
-                    " * <%= app_info.name %> - version <%= app_info.version %>:<%= build_mode %> - " +
-                    "<%= grunt.template.today('yyyy-mm-dd') %>\n" +
-                    " * <%= app_info.description %>\n" +
-                    " * (C) <%= grunt.template.today('yyyy') %> <%= app_info.author %>\n" +
-                    " */\n"
+                        " * <%= app_info.name %> - version <%= app_info.version %>:<%= mode %> - " +
+                        "<%= grunt.template.today('yyyy-mm-dd') %>\n" +
+                        " * <%= app_info.description %>\n" +
+                        " * (C) <%= grunt.template.today('yyyy') %> <%= app_info.author %>\n" +
+                        " */\n"
                 },
                 files: {
-                    src: [ "build/**/*.css" ]
+                    src: ["build/**/*.css"]
                 }
             },
 
             html: {
                 options: {
                     banner: "<!--\n" +
-                    " <%= app_info.name %> - version <%= app_info.version %>:<%= build_mode %> - " +
-                    "<%= grunt.template.today('yyyy-mm-dd') %>\n" +
-                    " <%= app_info.description %>\n" +
-                    " (C) <%= grunt.template.today('yyyy') %> <%= app_info.author %>\n" +
-                    "-->\n"
+                        " <%= app_info.name %> - version <%= app_info.version %>:<%= mode %> - " +
+                        "<%= grunt.template.today('yyyy-mm-dd') %>\n" +
+                        " <%= app_info.description %>\n" +
+                        " (C) <%= grunt.template.today('yyyy') %> <%= app_info.author %>\n" +
+                        "-->\n"
                 },
                 files: {
-                    src: [ "build/**/*.html" ]
+                    src: ["build/**/*.html"]
                 }
             },
 
             sh: {
                 options: {
                     banner: "#!/bin/bash\n" +
-                    "# <%= app_info.name %> - version <%= app_info.version %>:<%= build_mode %> - " +
-                    "<%= grunt.template.today('yyyy-mm-dd') %>\n" +
-                    "# <%= app_info.description %>\n" +
-                    "# (C) <%= grunt.template.today('yyyy') %> <%= app_info.author %>\n"
+                        "# <%= app_info.name %> - version <%= app_info.version %>:<%= mode %> - " +
+                        "<%= grunt.template.today('yyyy-mm-dd') %>\n" +
+                        "# <%= app_info.description %>\n" +
+                        "# (C) <%= grunt.template.today('yyyy') %> <%= app_info.author %>\n"
                 },
                 files: {
-                    src: [ "build/**/*.sh" ]
+                    src: ["build/**/*.sh"]
                 }
             }
         },
@@ -154,94 +107,51 @@ module.exports = function(grunt){
             "options": {
                 "failOnError": true
             },
-            "main": {
-            }
+            "main": {}
         },
 
         bump: {
             options: {
-                files: [ "package.json", "app_info.json", "src/client/bower.json", "src/server/package.json" ],
-                updateConfigs: [ "pkg", "app_info" ],
+                files: ["package.json", "app_info.json", "src/client/bower.json", "src/server/package.json"],
+                updateConfigs: ["pkg", "app_info"],
                 commit: true,
-                commitMessage: "Release v%VERSION%:" + (build_mode ? build_mode : "bump-only"),
-                commitFiles: [ "package.json", "app_info.json", "src/client/bower.json", "src/server/package.json" ],
+                commitMessage: "Release v%VERSION%:" + (mode ? mode : "bump-only"),
+                commitFiles: ["package.json", "app_info.json", "src/client/bower.json", "src/server/package.json"],
                 createTag: false,
                 push: false
             }
         },
 
-        rsync: {
+        synchard: {
             options: {
-                args: ["-p"],
+                mkdirp: true,
                 recursive: true
             },
-            app_dev: {
-                options: {
-                    src: "build/*",
-                    dest: "/var/node/simple-error-server",
-                    host: "node@localhost"
-                }
+            dev: {
+                files: [
+                    {src: "build/", dest: "node@localhost:/var/node/simple-error-server"},
+                    {src: "build/scripts/upstart/", dest: "node@localhost:/etc/init"},
+                    {src: "build/scripts/monit/", dest: "node@localhost:/etc/monit/conf.d"}
+                ]
             },
-            app_staging: {
-                options: {
-                    src: "build/*",
-                    dest: "/var/node/simple-error-server",
-                    host: "node@staginghost"
-                }
+            staging: {
+                files: [
+                    {src: "build/", dest: "node@staginghost:/var/node/simple-error-server"},
+                    {src: "build/scripts/upstart/", dest: "node@staginghost:/etc/init"},
+                    {src: "build/scripts/monit/", dest: "node@staginghost:/etc/monit/conf.d"}
+                ]
             },
-            app_prod: {
-                options: {
-                    src: "build/*",
-                    dest: "/var/node/simple-error-server",
-                    host: "node@prodhost"
-                }
-            },
-            upstart_dev: {
-                options: {
-                    src: "build/scripts/upstart/*",
-                    dest: "/etc/init",
-                    host: "node@localhost"
-                }
-            },
-            upstart_staging: {
-                options: {
-                    src: "build/scripts/upstart/*",
-                    dest: "/etc/init",
-                    host: "node@staginghost"
-                }
-            },
-            upstart_prod: {
-                options: {
-                    src: "build/scripts/upstart/*",
-                    dest: "/etc/init",
-                    host: "node@prodhost"
-                }
-            },
-            monit_dev: {
-                options: {
-                    src: "build/scripts/monit/*",
-                    dest: "/etc/monit/conf.d",
-                    host: "node@localhost"
-                }
-            },
-            monit_staging: {
-                options: {
-                    src: "build/scripts/monit/*",
-                    dest: "/etc/monit/conf.d",
-                    host: "node@staginghost"
-                }
-            },
-            monit_prod: {
-                options: {
-                    src: "build/scripts/monit/*",
-                    dest: "/etc/monit/conf.d",
-                    host: "node@prodhost"
-                }
+            prod: {
+                files: [
+                    {src: "build/", dest: "node@prodhost:/var/node/simple-error-server"},
+                    {src: "build/scripts/upstart/", dest: "node@prodhost:/etc/init"},
+                    {src: "build/scripts/monit/", dest: "node@prodhost:/etc/monit/conf.d"}
+                ]
             }
         },
 
         sshexec: {
-            restart_dev: {
+            dev: {
                 command: "echo restart NYI",
                 options: {
                     host: "localhost",
@@ -250,7 +160,7 @@ module.exports = function(grunt){
                     agent: process.env.SSH_AUTH_SOCK,
                 }
             },
-            restart_staging: {
+            staging: {
                 command: "echo restart NYI",
                 options: {
                     host: "localhost",
@@ -259,7 +169,7 @@ module.exports = function(grunt){
                     agent: process.env.SSH_AUTH_SOCK,
                 }
             },
-            restart_prod: {
+            prod: {
                 command: "echo restart NYI",
                 options: {
                     host: "prodhost",
@@ -271,29 +181,23 @@ module.exports = function(grunt){
         }
     });
 
-grunt.registerTask("write_version", function() {
-    grunt.event.once("git-describe", function(rev) {
-        grunt.file.write("build/version.json", JSON.stringify({
-            version: grunt.config("pkg.version") + (build_mode ? ":" + build_mode : ""),
-            revision: rev.object + (rev.dirty ? rev.dirty : "-clean") + (rev.tag ? "---" + rev.tag : ""),
-            date: grunt.template.today()
-        }));
+    grunt.registerTask("write_ver", function() {
+        grunt.event.once("git-describe", function(rev) {
+            grunt.file.write("build/version.json", JSON.stringify({
+                version: grunt.config("pkg.version") + (mode ? ":" + mode : ""),
+                revision: rev.object + (rev.dirty ? rev.dirty : "-clean") + (rev.tag ? "---" + rev.tag : ""),
+                date: grunt.template.today()
+            }));
+        });
+        grunt.task.run("git-describe");
     });
-    grunt.task.run("git-describe");
-});
 
-grunt.registerTask("build:dev", ["clean:total", /*"bump:build",*/ "copy:build", "copy:config_dev",
- "copy:override_dev", "mkdir:logs", "usebanner", "write_version", "clean:tidy"]);
-grunt.registerTask("build:staging", ["clean:total", "bump:build", "copy:build", "copy:config_staging",
- "copy:override_staging", "mkdir:logs", "usebanner", "write_version", "clean:tidy"]);
-grunt.registerTask("build:prod", ["clean:total", "bump:build", "copy:build", "copy:config_prod",
- "copy:override_prod", "mkdir:logs", "usebanner", "write_version", "clean:tidy"]);
-
-grunt.registerTask("deploy:dev", ["rsync:app_dev", "rsync:upstart_dev", "rsync:monit_dev"]);
-grunt.registerTask("deploy:staging", ["rsync:app_staging", "rsync:upstart_staging", "rsync:monit_staging"]);
-grunt.registerTask("deploy:prod", ["rsync:app_prod", "rsync:upstart_prod", "rsync:monit_prod"]);
-
-grunt.registerTask("build_and_deploy:dev" ["build:dev", "deploy:dev"]);
-grunt.registerTask("build_and_deploy:staging" ["build:staging", "deploy:staging"]);
-grunt.registerTask("build_and_deploy:prod" ["build:prod", "deploy:prod"]);
+    if(mode === "dev") {
+        var build_tasks = ["clean", "copy", "set_app_mode", "mkdir", "usebanner", "write_ver"];
+    }
+    else {
+        var build_tasks = ["clean", "bump", "copy", "set_app_mode", "mkdir", "usebanner", "write_ver"];
+    }
+    grunt.registerTask("build", build_tasks);
+    grunt.registerTask("deploy", ["synchard:" + mode, "sshexec:" + mode]);
 };
