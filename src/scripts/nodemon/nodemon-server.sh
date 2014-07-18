1fa180b886c03f13ea0d8519a4ce58d3cf591fa9
Christo Fogelberg <cgf@syntilect.com>
2014-07-18 19:19:32 +0100

    Tabs to spaces in all non-JS files

--- src/scripts/nodemon/nodemon-server.sh
+++ src/scripts/nodemon/nodemon-server.sh
@@ -3,32 +3,32 @@ set -e
 set -u
 
 usage() {
-	echo "Usage:"
-	echo "  lv.sh [server-mode, e.g. dev, staging, prod]"
-	echo "Also, ensure that nodemon is available in the path"
-	exit 1 
+  echo "Usage:"
+  echo "  nodemon-server.sh [server-mode, e.g. dev, staging, prod]"
+  echo "Also, ensure that nodemon is available in the path"
+  exit 1
 }
 
 if [ $# -eq 1 ]
 then
-	# Set up variables
-	MODE=$1
-	NODEMON_BIN="$(which nodemon 2> /dev/null)"
-	SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-	SERVER_DIR=$SCRIPT_DIR/../server
+  # Set up variables
+  MODE=$1
+  NODEMON_BIN="$(which nodemon 2> /dev/null)"
+  SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+  SERVER_DIR=$SCRIPT_DIR/../server
 
-	# Check path
-	if [ -z "$NODEMON_BIN" ]
-	then
-		echo "ERROR - nodemon must be on path"
-		usage
-		exit 1
-	fi
+  # Check path
+  if [ -z "$NODEMON_BIN" ]
+  then
+    echo "ERROR - nodemon must be on path"
+    usage
+    exit 1
+  fi
 
-	# Start server
-	cd $SERVER_DIR
-	echo Starting server in $(pwd)...
-	nodemon --delay 3 $SERVER_DIR/server.js $MODE 
+  # Start server
+  cd $SERVER_DIR
+  echo Starting server in $(pwd)...
+  nodemon --delay 3 $SERVER_DIR/server.js $MODE
 else
-	usage
+  usage
 fi
